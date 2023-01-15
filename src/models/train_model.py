8f9901bed952a4a554db52758db0f5b7fcbf8b32
palomaguiral <pguiort@gmail.com>
2023-01-15 13:09:30 +0100

    Confi file created

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,3 @@
-import timm 
 import opendatasets as od
 import numpy as np
 import os
@@ -14,7 +13,24 @@ import timm
 from torch import optim
 from model import Net
 
-def train():
+import logging
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+log = logging.getLogger(__name__)
+print = log.info #print --> log.info
+
+
+project_path=os.getcwd() #so hydra doesn't change the path 
+
+@hydra.main(config_path="../../config", config_name='default_config.yaml') #config/default_config.yaml
+def train(config: DictConfig):
+    #print(f"configuration: \n {OmegaConf.to_yaml(config)}")
+    logger = logging.getLogger(__name__)
+    logger.info("Start Training...")
+    
+    hparams = config.experiment.hyperparams #load the hyperparameters. config/experiment/exp1.yaml --> hyperparams
+
     train_transformer = transforms.Compose([
     transforms.Resize((150,150)),
     transforms.RandomHorizontalFlip(),
@@ -30,29 +46,27 @@ def train():
                             [0.5,0.5,0.5])
     ])
 
-    trainloader = DataLoader(torchvision.datasets.ImageFolder("data/raw/tomato-disease-multiple-sources/train",transform=train_transformer),batch_size=32,shuffle=True)
-    testloader = DataLoader(torchvision.datasets.ImageFolder("data/raw/tomato-disease-multiple-sources/valid",transform=test_transformer),batch_size=32,shuffle=True)
+    trainloader = DataLoader(torchvision.datasets.ImageFolder(project_path + '/data/raw/tomato-disease-multiple-sources/train', transform=train_transformer),batch_size=hparams["batch_size"],shuffle=True)
+    testloader = DataLoader(torchvision.datasets.ImageFolder(project_path  + '/data/raw/tomato-disease-multiple-sources/valid', transform=test_transformer),batch_size=hparams["batch_size"],shuffle=True)
 
     model = Net()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=hparams["lr"])
 
-        
-    n_epoch = 20
-    for epoch in range (n_epoch):
+       
+    for epoch in range (hparams["n_epoch"]):
         loss_tracker = []
-        contador=0
+        #contador=0
         for images, labels in trainloader:
-            print(contador)
+            #print(contador)
             optimizer.zero_grad()
             log_ps = model(images)
-            #print(log_ps)
             loss = criterion(log_ps, labels)
             loss.backward()
             optimizer.step()
             loss_tracker.append(loss.item())
-            contador+=1
+            #contador+=1
+        
 
-
-        print(f"Epoch {epoch+1}/{n_epoch}. Loss: {loss}")
+        print(f"Epoch {epoch+1} /{hparams['n_epoch']}. Loss: {loss}")
 train()
