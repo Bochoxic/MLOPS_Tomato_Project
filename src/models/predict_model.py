7df072acaf6f4a5a0b6caf899f2c62f66564badf
Victor Vigara <vigaravictor99@gmail.com>
2023-01-20 12:24:53 +0100

    predict_model updated and  model acuracy created

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,11 +5,11 @@ import cv2
 import torch.nn.functional as F
 
 def predict(model_checkpoint, image_path): 
-    labels_dict = {'0':'Bacterial spot' , '1':'Early blight' , '2':'healthy', '3': 'Late blight', '4': 'Leaf mold', 
-              '5': 'Powdery mildow', '6': 'Septoria leaf spot', '7': 'Spider mites', '8': 'Target spot', 
-              '9': 'Tomato mosaic virus', '10': 'Leaf curl virus' }
+    labels_dict = {'0':'Bacterial spot' , '1':'Early blight' , '2':'Late_blight', '3': 'Leaf mold', '4': 'Septoria leaf spot', 
+              '5': 'Spide mites', '6': 'Target spot', '7': 'Curl virus', '8': 'Mosaic virus', 
+              '9': 'healthy', '10': 'powdery mildew' }
     # Load model from checkpoint
-    model = Net(lr=0.0001)
+    model = Net(0.0001)
     model.eval()
     model.load_state_dict(torch.load(model_checkpoint))
 
@@ -30,4 +30,4 @@ def predict(model_checkpoint, image_path):
     probability = top_p.item()*100
     return probability, label
 
-predict('models/model.pkl', 'prueba.jpg')
+predict('models/lightning/trained_model.pt', 'prueba.jpg')
